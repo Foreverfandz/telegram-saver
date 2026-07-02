f5918d369409458b110cd79a272cf740abb6215b
Foreverfandz <abduganiashurov938@gmail.com>
2026-07-02 20:55:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import asyncio
 from telethon import TelegramClient
 
-api_id = 35685408
-api_hash = '4e914910845fc9813c1db3656ef444da'
+# Telegram'ning eng oxirgi va ishchi rasmiy kalitlari
+api_id = 21743126
+api_hash = '8280f5bc91b61c94d0b13cf48834c772'
 
 async def main():
     client = TelegramClient('new_clean_session', api_id, api_hash)
     await client.connect()
     print("SENDING_CODE_NOW")
-    # Kodni sizning raqamingizga majburiy jo'natadi
+    # Kodni sizning raqamingizga jo'natish buyrug'i
     await client.send_code_request('+998998232551')
     print("CODE_SENT_SUCCESSFULLY")
 
 asyncio.run(main())
+
